b7cef0184fdf205c133a393f6dbfd2b010e5d075
Philippe Charrière <ph.charriere@gmail.com>
2023-11-03 07:00:38 +0000

    💾 Saved.

--- snippets-install/install.sh
+++ snippets-install/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SNIPPETS_VERSION="0.0.1"
 SNIPPETS_OS="linux" # or darwin
-SNIPPETS_ARCH="arm64" # or amd64
+SNIPPETS_ARCH="amd64" # or amd64 arm64
 wget https://github.com/bots-garden/snippets/releases/download/v${SNIPPETS_VERSION}/snippets-v${SNIPPETS_VERSION}-${SNIPPETS_OS}-${SNIPPETS_ARCH}
 cp snippets-v${SNIPPETS_VERSION}-${SNIPPETS_OS}-${SNIPPETS_ARCH} snippets
 chmod +x snippets
